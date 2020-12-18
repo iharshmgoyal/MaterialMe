5b508245c5a4b00c98a9d193391359e9c3d74ffe
Harsh <goyalharsh7827@gmail.com>
2020-12-19 00:36:50 +0530

    MaterialMe

---
+++ app/src/main/java/com/example/android/materialme/DetailActivity.java
@@ -0,0 +1,38 @@
+package com.example.android.materialme;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.transition.TransitionInflater;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        TextView sportsTitle = findViewById(R.id.titleDetail);
+        ImageView sportsImage = findViewById(R.id.sportsImageDetail);
+        TextView sportsDetail = findViewById(R.id.subTitleDetail);
+        getWindow().setSharedElementEnterTransition(TransitionInflater.from(this)
+                .inflateTransition(R.transition.shared_element));
+
+        Intent intent = getIntent();
+        String titleString = intent.getStringExtra("title");
+        int imageInt = intent.getIntExtra("image_resource", 0);
+        String details = intent.getStringExtra("details");
+
+        sportsTitle.setText(titleString);
+        Glide.with(this).load(imageInt).into(sportsImage);
+        sportsDetail.setText(details);
+    }
+}
